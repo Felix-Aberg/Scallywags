57ca5d3c436b5c6f619838af78b7021b0904d00d
Felix <felixaberg1999@gmail.com>
2020-04-07 11:55:24 +0200

    Created Input handler + project inputs

---
+++ Assets/Scripts/PlayerComponents/InputHandler.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScallyWags
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public float GetXAxis(int index)
+        {
+            if (index < 1 || index > 4)
+            {
+                Debug.LogError("Array out of index in InputHandler GetXAxis. Array ranges from 1-4. Index: " + index);
+                return 0;
+            }
+            else
+            {
+                return Input.GetAxis("Horizontal" + index);
+            }
+
+        }
+
+        public float GetYAxis(int index)
+        {
+            if (index < 1 || index > 4)
+            {
+                Debug.LogError("Array out of index in InputHandler GetYAxis. Array ranges from 1-4. Index: " + index);
+                return 0;
+            }
+            else
+            {
+                return Input.GetAxis("Vertical" + index);
+            }
+        }
+
+        public bool GetPickup(int index)
+        {
+            if (index < 1 || index > 4)
+            {
+                Debug.LogError("Array out of index in InputHandler GetPickup. Array ranges from 1-4. Index: " + index);
+                return false;
+            }
+            else
+            {
+                return Input.GetButton("Pickup" + index);
+            }
+        }
+
+        public bool GetInteract(int index)
+        {
+            if (index < 1 || index > 4)
+            {
+                Debug.LogError("Array out of index in InputHandler GetInteract. Array ranges from 1-4. Index: " + index);
+                return false;
+            }
+            else
+            {
+                return Input.GetButton("Interact" + index);
+            }
+        }
+
+        public bool GetJump(int index)
+        {
+            if (index < 1 || index > 4)
+            {
+                Debug.LogError("Array out of index in InputHandler GetJump. Array ranges from 1-4. Index: " + index);
+                return false;
+            }
+            else
+            {
+                return Input.GetButton("Jump" + index);
+            }
+        }
+
+        public bool GetYarr(int index)
+        {
+            if (index < 1 || index > 4)
+            {
+                Debug.LogError("Array out of index in InputHandler GetYarr. Array ranges from 1-4. Index: " + index);
+                return false;
+            }
+            else
+            {
+                return Input.GetButton("Yarr" + index);
+            }
+        }
+    }
+}
