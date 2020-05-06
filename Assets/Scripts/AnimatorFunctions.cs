7c82f935285f416e1c534beea328c104908ec683
Oscar Svan <oscarsvan@hotmail.com>
2020-05-06 16:15:01 +0200

    Added fonts, polished up main menu, updated fonts everywhere

---
+++ Assets/Scripts/AnimatorFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+    [SerializeField] MainMenu mainMenu;
+    public bool disableOnce;
+
+    void PlaySound(AudioClip whichSound)
+    {
+        if (!disableOnce)
+        {
+            mainMenu.audioSource.PlayOneShot(whichSound);
+        }
+        else
+        {
+            disableOnce = false;
+        }
+    }
+}
