c5128a08c1eccd5edcac6f54200b0aab3dce565a
Felix <felixaberg1999@gmail.com>
2020-05-07 11:57:06 +0200

    Dirty code for film studio
    Does not affect the other scenes

---
+++ Assets/Scripts/Tests/FilmStudioPlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScallyWags
+{
+    public class FilmStudioController : MonoBehaviour
+    {
+        public List<GameObject> _playerGameObjects;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            int i = 0;
+            foreach (GameObject player in _playerGameObjects)
+            {
+                i++;
+                player.GetComponent<Player>().Init(i);
+            }
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            foreach (GameObject player in _playerGameObjects)
+            {
+                player.GetComponent<Player>().Tick();
+            }
+        }
+    }
+}
