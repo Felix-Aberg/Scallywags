da63a9f301298b3f4986cf76068649e22c85c3ad
Oscar Svan <oscarsvan@hotmail.com>
2020-05-15 15:45:19 +0200

    Added Wave Transition to Main Menu

--- Assets/Scripts/Utilities/LoadScene.cs
+++ Assets/Scripts/Utilities/LoadScene.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    public Animator transition;
+    public float transitionTime = 1.3f;
+
     public void LoadSceneByName(string sceneName)
     {
+        transition.SetTrigger("Start");
+        StartCoroutine(WaitTime(sceneName));
+        
+    }
+
+    IEnumerator WaitTime(string sceneName)
+    {
+        yield return new WaitForSeconds(transitionTime);
         SceneManager.LoadScene(sceneName);
     }
+
+    
 }
