07e98de9a8a80f2f33fce76c0182c963cbd79ce4
Jari <madcook86@gmail.com>
2020-05-21 10:21:36 +0200

    Fix return to menu from character screen

---
+++ Assets/Scripts/UI/ReturnToMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    private LoadScene _loadScene;
+    [SerializeField] private string _sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _loadScene = FindObjectOfType<LoadScene>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _loadScene.LoadSceneByName(_sceneName);
+        }
+    }
+}
