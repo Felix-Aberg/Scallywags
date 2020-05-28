1b2ffc836f3d1a1f80e5ea4285629c82d4f6b0a7
Oscar Svan <oscarsvan@hotmail.com>
2020-05-28 16:35:19 +0200

    Loading Screen has Delay, press any to continue

--- Assets/Scripts/Utilities/LoadingScreen.cs
+++ Assets/Scripts/Utilities/LoadingScreen.cs
@@ -2,22 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LoadingScreen : MonoBehaviour
 {
     [SerializeField] private string sceneName;
     public Animator transition;
     public float transitionTime = 1.3f;
+    bool isLoaded = false;
+    public GameObject loadingText;
+    public GameObject continueText;
+    public GameObject loadingBoat;
+    bool once;
     void Start()
     {
-        StartCoroutine(LoadNextScene(sceneName));
+        StartCoroutine(LoadNextScene());
     }
 
-    private IEnumerator LoadNextScene(string sceneName)
+    private void Update()
     {
+        if (!once && isLoaded && Input.anyKeyDown)
+        {
+            once = true;
+            Debug.Log("button pressed");
+            transition.SetTrigger("Start");
+            StartCoroutine(ActualLoadNextScene(sceneName));
+        }
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        Debug.Log("LoadNextScene Logged");
         yield return new WaitForSeconds(5f);
-        transition.SetTrigger("Start");
+        isLoaded = true;
+        loadingText.SetActive(false);
+        loadingBoat.SetActive(false);
+        continueText.SetActive(true);
+
+    }
+
+    private IEnumerator ActualLoadNextScene(string sceneName)
+    {
         yield return new WaitForSeconds(transitionTime);
         SceneManager.LoadScene(sceneName);
+        
     }
 }
