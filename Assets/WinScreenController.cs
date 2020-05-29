b6fe028b9731487a7e1aa5ba4e059d32ef1f7740
Oscar Svan <oscarsvan@hotmail.com>
2020-05-29 18:23:46 +0200

    Win screen and Lose screen stuff
    I still need to add a background to the win screen but then that's it!!!

---
+++ Assets/WinScreenController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreenController : MonoBehaviour
+{
+    [SerializeField] private string _mainMenu;
+    private LoadScene _loadScene;
+    private bool canPressContinue;
+    public Image panel;
+    public GameObject text;
+    public GameObject anyToContinue;
+    public float fadeTime;
+    
+    void Start()
+    {
+        panel = transform.Find("Panel").GetComponent<Image>();
+        text = transform.Find("Panel").Find("Text").gameObject;
+        anyToContinue = transform.Find("Panel").Find("Any To Continue").gameObject;
+        Color blank = new Color(255f, 255f, 255f, 0f);
+        panel.color = blank;
+        text.SetActive(false);
+        anyToContinue.SetActive(false);
+        _loadScene = GetComponentInChildren<LoadScene>();
+        StartCoroutine(WaitTime());
+    }
+
+    private void Update()
+    {
+        if (canPressContinue)
+        {
+            Color temp = panel.color;
+            temp.a += fadeTime * Time.deltaTime;
+            panel.color = temp;
+            
+            if (temp.a > 0.99f)
+            {
+                text.SetActive(true);
+                anyToContinue.SetActive(true);
+            }
+
+            if (Input.anyKeyDown)
+            {
+                _loadScene.LoadSceneByName("MainMenu");
+            }
+        }
+
+
+    }
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(4);
+        canPressContinue = true;
+    }
+
+   
+}
